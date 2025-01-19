6d0ff53e24b72ddb88ec05b218b78441092702cd
Cinzano97 <78725827+Cinzano97@users.noreply.github.com>
2025-01-19 16:40:02 +0700

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,6 @@
+from note_manager.greetings import title
+title1 = input('Введите заголовок: ')
+title2 = input('Введите заголовок: ')
+title3 = input('Введите заголовок: ')
+catalog = [title1, title2, title3]
+print(catalog)
