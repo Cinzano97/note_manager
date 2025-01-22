e2fe8262c502bcac745f83bff3b529ad5db8a86d
Cinzano97 <78725827+Cinzano97@users.noreply.github.com>
2025-01-22 21:13:10 +0700

    Add files via upload

---
+++ final.py
@@ -0,0 +1,13 @@
+from note_manager.add_input import username, content, status, created_date, issue_date
+from note_manager.add_list import title1, title2, title3
+
+roll = [
+    username,
+    content,
+    status,
+    created_date,
+    issue_date,
+    [title1, title2, title3]
+
+]
+print(roll)
